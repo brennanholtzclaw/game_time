50a02c00125b383eb5859ea53c820a1e90c191f7
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-17 08:06:59 -0600

    Test written but still failing
    The test isn't finding the development .js file as far as I can tell.

---
+++ test/index.js
@@ -0,0 +1,15 @@
+describe('ball', function() {
+
+  it('should move each game loop', function() {
+    var ball = new Ball(25, 25, 5)
+    assert(ball.x == 5)
+    assert(ball.y == 5)
+
+    for(i=0;i<ball.x;i++) {
+      ball.move();
+    };
+
+    assert(ball.x == 20)
+    assert(ball.y == 20)
+  });
+});
