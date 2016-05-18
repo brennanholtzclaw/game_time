56d0482c0a5646ef50babca3200276878b7874b6
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-18 08:41:20 -0600

    Attempt to require js files

--- test/index.js
+++ test/index.js
@@ -1,9 +1,16 @@
 // require ball js file
 
+//= require ../lib/index.js
+
 describe('ball', function() {
 
   it('should move each game loop', function() {
+    var canvas = function(){
+      x = 10,
+      y = 10
+    }
     var ball = new Ball(25, 25, 5)
+
     assert(ball.x == 5)
     assert(ball.y == 5)
 
