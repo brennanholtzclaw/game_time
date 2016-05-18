fbb3b5acd71b3934765f02a2989413bee98260bd
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-18 10:05:12 -0600

    pull ball-test into seperate file'

---
+++ test/ball-test.js
@@ -0,0 +1,24 @@
+var chai = require('chai');
+var assert = chai.assert;
+
+var Ball = require('../lib/index');
+
+describe('ball', function() {
+  it('should move each game loop', function() {
+    var canvas = function(){
+      x = 10,
+      y = 10
+    }
+    var ball = new Ball(25, 25, 5)
+
+    assert(ball.x == 5)
+    assert(ball.y == 5)
+
+    for(i=0;i<ball.x;i++) {
+      ball.move();
+    };
+
+    assert(ball.x == 20)
+    assert(ball.y == 20)
+  });
+});
