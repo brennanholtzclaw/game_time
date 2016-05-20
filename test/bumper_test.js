86d1b67005157a5937403f602ae9467fcd96fba8
hhoopes <heidih@gmail.com>
2016-05-19 22:01:08 -0600

    Add tests for hole, bumper

---
+++ test/bumper_test.js
@@ -0,0 +1,30 @@
+const chai = require('chai')
+const assert = chai.assert;
+const sinon = require('sinon');
+const Bumper = require("../lib/bumper")
+
+describe("Bumper", function(){
+  context("with assigned attributes", function(){
+    var bumper = new Bumper(0,0,10,10)
+
+    it("should have an x min position", function(){
+      assert.equal(bumper.minX, 0)
+    })
+
+    it("should have a y min position", function(){
+      assert.equal(bumper.minY, 0)
+    })
+
+    it("should have an x max position", function(){
+      assert.equal(bumper.maxX, 10)
+    })
+
+    it("should have a y max position", function(){
+      assert.equal(bumper.maxY, 10)
+    })
+
+    it("should have a default color of white", function(){
+      assert.equal(bumper.color, "white")
+    });
+  });
+});
