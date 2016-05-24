d90396cdd683ad6c1b91654d9f1c3f1668f636d6
hhoopes <heidih@gmail.com>
2016-05-23 18:34:36 -0600

    Add update method

---
+++ test/game_test.js
@@ -0,0 +1,33 @@
+var chai = require('chai')
+var assert = chai.assert;
+var sinon = require('sinon');
+var Game = require('../lib/game')
+var Level = require('../lib/level');
+
+describe("Game", function(){
+  context("with assigned attributes", function(){
+    var game = new Game;
+
+    it("defaults to level 1", function(){
+      console.log(game)
+      assert.equal(game.currentLevel.number, 1)
+    })
+
+    it("can update its level to another level", function(){
+      game.updateLevel(0)
+
+      assert.equal(game.currentLevel.number, 0)
+    })
+
+    it("can update its level attributes to another level's", function(){
+      var level = new Level(0);
+
+      assert.notEqual(game.currentLevel.ball.x, level.ball.x)
+
+      game.updateLevel(1)
+
+      assert.equal(game.currentLevel.ball.x, level.ball.x)
+      assert.equal(game.currentLevel.par, level.par)
+    })
+  })
+})
