d5121ca5409a0be173dfa7cfdd38bd86ce20d9dd
Brennan Holtzclaw <brennanholtzclaw@gmail.com>
2016-05-23 22:27:39 -0600

    Write tests for hitting ball - delete comments

--- test/hitting_the_ball_test.js
+++ test/hitting_the_ball_test.js
@@ -4,26 +4,36 @@ var sinon = require('sinon');
 var Ball = require("../lib/ball")
 var Club = require("../lib/club")
 
-// hit the ball once - make sure its where it should be
-//
-// hit the ball again - same check
+describe('Checking ball direction', function(){
+  it("goes in the right direction", function(){
+    var clubX = 1
+    var clubY = 1
+    var ballX = 5
+    var ballY = 5
 
-describe('Hitting the ball a second time', function(){
-  xit("goes in the right direction", function(){
-    var clX = 118
-    var clY = 309
-    var bllX =  119.93382034811994
-    var bllY = 380.9117913908383
-
-    var ball = new Ball( bllX,
-                        bllY,
-                        6)
-    var club = new Club(0, ball, clX, clY);
-    var slopeDeets = club.clubToBallSlope({x: clX, y: clY});
+    var ball = new Ball({x: ballX, y: ballY, radius: 6})
+    var club = new Club(0, ball, clubX, clubY);
+    var slopeDeets = club.clubToBallSlope({x: clubX, y: clubY});
     assert(slopeDeets.bothSlope > 0, 'the slope is ' + slopeDeets.bothSlope);
-    assert(ball.xSpeed < 0, 'xSpeed greater than zero')
-    // var slopeDeets = club.clubToBallSlope({x: clX, y: clY});
-    // assert(slopeDeets.bothSlope > 0, 'the slope is ' + slopeDeets.bothSlope);
-    // assert(ball.xSpeed > 0 && ball.xDirection > 0, 'this direction should be positive');
+    assert(ball.xSpeed > 0, 'xSpeed less than zero')
+  })
+
+  it("direction reverts to positive after stop", function(){
+    var clubX = 1
+    var clubY = 1
+    var ballX = 5
+    var ballY = 5
+
+    var ball = new Ball({x: ballX, y: ballY, radius: 6})
+    var club = new Club(0, ball, clubX, clubY);
+
+    ball.xSpeed = -.01
+    ball.ySpeed = -.01
+
+    ball.move()
+    // at the end of the move function the ball should encounter stopCheck which should flip direction back to positive
+
+    assert(ball.xSpeed > 0, "ball x speed less than 0")
+    assert(ball.ySpeed > 0, "ball y speed less than 0")
   })
 })
