afc3f915b9012a60ca9fb13d731ad88605c6af66
Nathan Venn <natevenn@gmail.com>
2016-05-25 14:19:52 -0600

    break loop when ball in hole on last level

---
+++ test/score_calculator_test.js
@@ -0,0 +1,16 @@
+//var chai = require('chai')
+//var assert = chai.assert;
+//var sinon = require('sinon');
+//var scoreCalculator = require('../lib/scoreCalculator');
+
+//describe('scoreCalculator', function() {
+    //context('scoring level', function() {
+        //var par = 2
+        //var strokes = 4
+        //var score = scoreCalculator.levelScore(strokes, par);
+
+        //it("should subtract stroke from par", function() {
+            //assert.equal(score, 2)
+        //});
+    //});
+//});
