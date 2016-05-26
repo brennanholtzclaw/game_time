78efc2b8bcd2db8489cb4aaed85cf66e957a58f5
hhoopes <heidih@gmail.com>
2016-05-26 14:03:55 -0600

    Build

--- main.bundle.js
+++ main.bundle.js
@@ -2140,7 +2140,7 @@
 
 	var Bumper = __webpack_require__(4);
 
-	var bumpersOne = [{ minX: 200, minY: 50, maxX: 500, maxY: 60, type: "bumper" }, { minX: 200, minY: 450, maxX: 500, maxY: 460, type: "bumper" }, { minX: 200, minY: 50, maxX: 210, maxY: 450, type: "bumper" }, { minX: 500, minY: 50, maxX: 510, maxY: 460, type: "bumper" }, { minX: 275, minY: 150, maxX: 425, maxY: 160, color: "blue", type: "bumper" }, { minX: 350, minY: 150, maxX: 360, maxY: 360, color: "blue", type: "bumper" }, { minX: 210, minY: 400, maxX: 260, maxY: 450, type: "sand" }, { minX: 450, minY: 400, maxX: 500, maxY: 450, type: "sand" }];
+	var bumpersOne = [{ minX: 200, minY: 50, maxX: 500, maxY: 60, type: "bumper" }, { minX: 200, minY: 450, maxX: 500, maxY: 460, type: "bumper" }, { minX: 200, minY: 50, maxX: 210, maxY: 450, type: "bumper" }, { minX: 500, minY: 50, maxX: 510, maxY: 460, type: "bumper" }, { minX: 275, minY: 150, maxX: 425, maxY: 160, color: "#404040", type: "bumper" }, { minX: 350, minY: 150, maxX: 360, maxY: 360, color: "#404040", type: "bumper" }, { minX: 210, minY: 400, maxX: 260, maxY: 450, type: "sand" }, { minX: 450, minY: 400, maxX: 500, maxY: 450, type: "sand" }];
 
 	module.exports = bumpersOne;
 
